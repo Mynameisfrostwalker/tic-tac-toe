625f13bdab1990d3feb67caf0dc73955de7b10aa
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-07-15 00:44:38 +0100

    added click event

--- scripts/script.js
+++ scripts/script.js
@@ -20,9 +20,39 @@ const displayControl = (
     function() {
         let x = 0;
         const ths = document.querySelectorAll('th');
-        ths.forEach(th => {
-            th.textContent = gameBoard.gameBoardArr[x];
-            x++
-        })
+        const update = () => {
+            ths.forEach(th => {
+                th.textContent = gameBoard.gameBoardArr[x];
+                x++
+            })
+            x = 0;
+        }
+        return {
+            update
+        }
     }
 )()
+
+const addMark = (
+    function() {
+        let x = 1;
+        const fun = (event) => {
+            let index = parseInt(event.target.classList.value) - 1;
+            if (gameBoard.gameBoardArr[index] === null) {
+                if (x % 2 === 0) {
+                    gameBoard.gameBoardArr[index] = "x"
+                } else {
+                    gameBoard.gameBoardArr[index] = "o"
+                }
+                x++
+            }
+            displayControl.update()
+        }
+        return { fun }
+    }
+)()
+
+const buttons = document.querySelectorAll('th');
+buttons.forEach(button => {
+    button.addEventListener('click', addMark.fun)
+})
